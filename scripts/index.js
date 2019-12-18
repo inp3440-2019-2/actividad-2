9d66fb36aab1b744fc92af9550a9848415c22ca8
Sebastián Vicencio <sivicencio@gmail.com>
2019-12-18 00:16:36 -0300

    feat(JS): add clap buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const topButton = document.querySelector('.scroll-top');
+const clapButtons = document.querySelectorAll('.clap-button');
   
 function scrollToTop() {
   window.scrollTo({
@@ -7,4 +8,15 @@ function scrollToTop() {
   });
 }
 
+function addClap(event) {
+  event.preventDefault();
+  const clapElem = event.currentTarget.querySelector('span');
+  const clapCount = Number(clapElem.textContent);
+  clapElem.textContent = clapCount + 1;
+}
+
 topButton.addEventListener('click', scrollToTop);
+
+clapButtons.forEach(function(clapButton) {
+  clapButton.addEventListener('click', addClap);
+});
